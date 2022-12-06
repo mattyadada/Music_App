123f1e2336a6f4930d1f68b2d9a19c6979edf976
mattyadada <matthewmc96@gmail.com>
2022-12-05 21:54:35 -0500

    wip

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+float getSales();
+float getAdvancedPay();
+float determineCommissionRate(double sales);
+
+
+int main()
+{
+    float realSales, commissionRate, advancedPay;
+
+    sales = getSales();
+
+}
+
+float getSales()
+{
+    float monthlySales;
+
+    printf("Enter the salesperson's monthly sales.");
+    scanf("%f", &monthlySales);
+
+    return monthlySales;
+}
+
+float getAdvancedPay()
+{
+    float advanced;
+
+    printf("Enter the amount of advanced pay, or 0 if no advanced pay was given.");
+    scanf("%f", &advanced);
+
+    return advanced;
+}
+
+float determineCommissionRate(double sales)
+{
+    float rate = 0;
+
+    if(sales < 10000.00){
+        rate == 0.10;
+    }else if(sales >= 10000.00 && sales <= 14999.99){
+        rate == 0.12;
+    }else if(sales >= 15000.00 && sales <= 17999.99){
+        rate == 0.14;
+    }else if(sales >= 18000.00 && sales <= 21999.99){
+        rate == 0.16;
+    }else{
+        rate == 0.18;
+    }
+    return rate;
+}
